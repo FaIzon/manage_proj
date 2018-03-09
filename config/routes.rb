c138713f9f5b7b17f7f374c700616f9eba24fe94
FaIzon <dimer90@gmail.com>
2018-03-09 11:18:08 +0700

    Add pages for confirm

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-
   as :user do
-      put '/user/confirmation' => 'confirmation#update', :via => :patch, :as => :update_user_confirmation
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
   end
 
-  devise_for :users, controllers: {
+   devise_for :users, controllers: {
     registrations: 'registrations',
-    confirmation: 'confirmation'
+    confirmations: 'confirmations'
   }
-  root 'home#index'
+
+root 'home#index'
 
 end
