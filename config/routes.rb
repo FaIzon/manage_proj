523d96f5a89af06d3c44f1e72e02fe8d15b012c6
FaIzon <dimer90@gmail.com>
2018-03-10 09:17:07 +0700

    Add Teams and Projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :teams
   as :user do
     put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
   end
 
-   devise_for :users, controllers: {
+  devise_for :users, controllers: {
     registrations: 'registrations',
     confirmations: 'confirmations'
   }
 
-root 'home#index'
-
+  root 'home#index'
 end
